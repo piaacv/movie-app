99506a58fa7e1e3d8849f69e406416a4d1632312
piaacv <122986202+piaacv@users.noreply.github.com>
2023-08-18 12:10:36 -0400

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-app';
+  http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get('https://api.themoviedb.org/3/discover/movie')
+    .subscribe((data:any) => {
+      console.log(data);
+    })
+  }
 }
